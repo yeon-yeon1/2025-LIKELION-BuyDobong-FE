a7823543dd4bcb70767b5da16dcac0fa5be4fa63
yeon-yeon1 <nojk0106@naver.com>
2025-08-16 04:33:15 +0900

    🐛 Fix: Header 기능 수정 (#4)
    Fix: Header 기능 수정 (#4)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import * as H from '@styles/HeaderStyle';
 import BackButton from '@assets/BackButton.svg?react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Header() {
   const location = useLocation();
+  const navigate = useNavigate();
   return (
     <>
       <H.Header>
         {location.pathname !== '/customerHome' && location.pathname !== '/merchantHome' && (
-          <BackButton />
+          <BackButton onClick={() => navigate(-1)} />
         )}
       </H.Header>
     </>

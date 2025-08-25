9c663e9d1ef4b734142c1567af7c1dea8eb50dd5
chldsbdud <cyy2532@gmail.com>
2025-08-25 18:23:11 +0900

    ✨ Feat: 상점 상세 페이지 이미지 띄우기

--- src/routes/customer/MarketDetail.tsx
+++ src/routes/customer/MarketDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '@components/Header';
 import * as S from '@styles/customer/MarketDetailStyle';
